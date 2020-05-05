c5092aa3bf638e193a836d1726a6399f7976e8e3
Charles <charles@what2do.sg>
2020-05-05 12:14:43 +0800

    [charles] added Register and Login

---
+++ src/components/Login.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import axios from "axios";
+// import { setUserSession } from "./Utils/Common";
+const baseURL = "http://localhost:3001/user";
+
+function Login(props) {
+  const [loading, setLoading] = useState(false);
+  const username = useFormInput("");
+  const password = useFormInput("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = () => {
+    setError(null);
+    setLoading(true);
+    axios({
+      method: "post",
+      url: baseURL + "/login",
+      data: {
+        username: username.value,
+        password: password.value,
+      },
+    })
+      .then((response) => {
+        setLoading(false);
+        props.history.push("/game");
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (!!error.response) {
+          if (error.response.status === 400) {
+            console.log(error.response.data);
+            setError(JSON.stringify(error.response.data));
+          } else {
+            setError("Something went wrong. Please try again later.");
+          }
+        } else {
+          setError("Something went wrong. Please try again later.");
+        }
+      });
+  };
+
+  const gotoRegister = () => {
+    props.history.push("./register");
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <br />
+      <br />
+      <div>
+        Username
+        <br />
+        <input type="text" {...username} autoComplete="new-password" />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        Password
+        <br />
+        <input type="password" {...password} autoComplete="new-password" />
+      </div>
+      {error && (
+        <>
+          <small style={{ color: "red" }}>{error}</small>
+          <br />
+        </>
+      )}
+      <br />
+      <input
+        type="button"
+        value="Register"
+        onClick={gotoRegister}
+        disabled={loading}
+      />
+      <input
+        type="button"
+        value={loading ? "Loading..." : "Login"}
+        onClick={handleLogin}
+        disabled={loading}
+      />
+      <br />
+    </div>
+  );
+}
+
+const useFormInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  return {
+    value,
+    onChange: handleChange,
+  };
+};
+
+export default Login;
---
+++ src/components/Register.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import axios from "axios";
+// import { setUserSession } from "./Utils/Common";
+const baseURL = "http://localhost:3001/user";
+
+function Register(props) {
+  const [loading, setLoading] = useState(false);
+  const username = useFormInput("");
+  const password = useFormInput("");
+  const repeatPassword = useFormInput("");
+  const [error, setError] = useState(null);
+
+  const handleRegister = () => {
+    setError(null);
+    setLoading(true);
+    if (password.value !== "" && password.value === repeatPassword.value) {
+      axios({
+        method: "post",
+        url: baseURL + "/register",
+        data: {
+          username: username.value,
+          password: password.value,
+        },
+      })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          setLoading(false);
+          if (!!error.response) {
+            if (error.response.status === 401) {
+              setError(JSON.stringify(error.response.data));
+            } else {
+              setError("Something went wrong. Please try again later.");
+            }
+          } else {
+            setError("Something went wrong. Please try again later.");
+          }
+        });
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <br />
+      <br />
+      <div>
+        Username
+        <br />
+        <input type="text" {...username} />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        Password
+        <br />
+        <input type="password" {...password} />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        Repeat Password
+        <br />
+        <input type="password" {...repeatPassword} />
+      </div>
+      {error && (
+        <>
+          <small style={{ color: "red" }}>{error}</small>
+          <br />
+        </>
+      )}
+      <br />
+      <input
+        type="button"
+        value={loading ? "Registering..." : "Register"}
+        onClick={handleRegister}
+        disabled={loading}
+      />
+      <br />
+    </div>
+  );
+}
+
+const useFormInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  return {
+    value,
+    onChange: handleChange,
+  };
+};
+
+export default Register;
